d8436a67a40ff46a71bc06ea6ddef3ee5ef96ff7
Kartikay Jainwal <kartikayjainwal@gmail.com>
2023-11-01 19:29:17 +0000

    fix: no duedate bugfix

--- src/utils/filters.py
+++ src/utils/filters.py
@@ -6,17 +6,28 @@ def key_for_task(ttTask):
     return '{} {}'.format(ttTask['title'], ttTask['list_name'])
 
 def is_today(ttTask):
+    if ttTask.get('dueDate', None) is None:
+        return False
     dtime = datetime.strptime(ttTask['dueDate'], '%Y-%m-%dT%H:%M:%S.%f%z')
     return dtime.date() == datetime.now().date()
 
 def is_tomorrow(ttTask):
+    if ttTask.get('dueDate', None) is None:
+        return False
     dtime = datetime.strptime(ttTask['dueDate'], '%Y-%m-%dT%H:%M:%S.%f%z')
     return dtime.date() == datetime.now().date() + timedelta(days=1)
 
 def is_this_week(ttTask):
+    if ttTask.get('dueDate', None) is None:
+        return False
     dtime = datetime.strptime(ttTask['dueDate'], '%Y-%m-%dT%H:%M:%S.%f%z')
     return dtime.date() >= datetime.now().date() and dtime.date() <= datetime.now().date() + timedelta(days=7)
 
 def sort_by_due_date(ttTask):
+    due_date = ttTask.get('dueDate', None)
+    if due_date is None:
+        return 9999999999
     dtime = datetime.strptime(ttTask['dueDate'], '%Y-%m-%dT%H:%M:%S.%f%z')
     return dtime.timestamp()
+
+
